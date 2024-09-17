230491884702ca24dbc1bde46e3857c7704a0c5e
zinksterz <zehngameplay@gmail.com>
2024-09-17 15:13:41 -0500

    Credentials error has been resolved - no longer necessary to login for every push

--- problems/day-one.js
+++ problems/day-one.js
@@ -1,6 +1,1 @@
-console.log("Connections being made");
-console.log("nothing wrong here");
-console.log("Understanding gitbash a lil better");
-console.log("This will be the test");
-console.log("This may have worked!");
-
+console.log("Credentials problem officially resolved and it's go time!");

b4298d65de80c018e10ad9bead35905754e691e9
zinksterz <zehngameplay@gmail.com>
2024-09-17 15:12:48 -0500

    Test push

--- problems/day-one.js
+++ problems/day-one.js
@@ -2,3 +2,5 @@ console.log("Connections being made");
 console.log("nothing wrong here");
 console.log("Understanding gitbash a lil better");
 console.log("This will be the test");
+console.log("This may have worked!");
+
